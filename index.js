f6fd926f154ea7ebecdf00052d61cfacbc78ef24
Alexandra Hansma <ahansma@butler.edu>
2017-05-15 16:00:20 -0400

    Add hair color

--- index.js
+++ index.js
@@ -7,9 +7,16 @@ personForm.onsubmit = (ev) =>
     const details = document.querySelector(".details")
 
     const personName = form.personName.value
+    const hairColor = form.hairColor.value
 
     const em = document.createElement('em') //element name in arguments
     em.textContent = personName
 
+    const colorDiv = document.createElement('div') //element name in arguments
+    colorDiv.style.backgroundColor = hairColor
+    colorDiv.style.height = '50px'
+    colorDiv.style.width = '100px'
+
     details.appendChild(em)
+    details.appendChild(colorDiv)
 }
