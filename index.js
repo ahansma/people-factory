8b9ee7db1bd7090380f66601e0f8a05b57dc02c9
Alexandra Hansma <ahansma@butler.edu>
2017-05-16 08:56:16 -0400

    Use a named function for the submit handler and use add event listener

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 const personForm = document.querySelector('form')
 
-personForm.onsubmit = (ev) =>
-{
-    ev.preventDefault()
-    const form = ev.target
-    const details = document.querySelector(".details")
-
-    const personName = form.personName.value
-    const hairColor = form.hairColor.value
-
-    const em = document.createElement('em') //element name in arguments
-    em.textContent = personName
-
-    const colorDiv = document.createElement('div') //element name in arguments
-    colorDiv.style.backgroundColor = hairColor
-    colorDiv.style.height = '50px'
-    colorDiv.style.width = '100px'
-
-    details.appendChild(em)
-    details.appendChild(colorDiv)
+const renderColor = (hairColor) => {
+  const colorDiv = document.createElement('div')
+  colorDiv.style.backgroundColor = hairColor
+  colorDiv.style.height = '50px'
+  colorDiv.style.width = '100px'
+  return colorDiv
 }
+
+const handleSubmit = (ev) => {
+  ev.preventDefault()
+  const form = ev.target
+  const details = document.querySelector('.details')
+
+  const personName = form.personName.value
+  const hairColor = form.hairColor.value
+  const age = form.age.value
+  const birthplace = form.birthplace.value
+
+  const colorDiv = renderColor(hairColor)
+
+  details.innerHTML = `
+    <ul>
+      <li>Name: ${personName}</li>
+      <li>Hair Color: ${colorDiv.outerHTML}</li>
+      <li>Age: ${age}</li>
+      <li>Birthplace: ${birthplace}</li>
+    </ul>
+  `
+}
+
+personForm.addEventListener('submit', handleSubmit)
