ccd387c71be5c4ed00c3fdc4a671b48613b74ad1
Alexandra Hansma <ahansma@butler.edu>
2017-05-16 15:22:42 -0400

    Wrap everything in an object with methods

--- index.js
+++ index.js
@@ -1,61 +1,50 @@
 
+const App = {
+  init() {
+    const personForm = document.querySelector('form')
+    personForm.addEventListener('submit', (ev) => this.handleSubmit(ev))
+  },
 
-{
+  renderColor(hairColor) {
+    const colorDiv = document.createElement('div')
+    colorDiv.style.backgroundColor = hairColor
+    colorDiv.style.height = '50px'
+    colorDiv.style.width = '100px'
+    return colorDiv
+  },
 
-const personForm = document.querySelector('form')
+  renderItem(name, value) {
+    const item = document.createElement('li')
+    item.innerHTML = `${name}: ${value}`
+    return item
+  },
 
-const renderColor = (hairColor) => {
-  const colorDiv = document.createElement('div')
-  colorDiv.style.backgroundColor = hairColor
-  colorDiv.style.height = '50px'
-  colorDiv.style.width = '100px'
-  return colorDiv
-}
-
-const renderItem = (name, value) => {
-  const item = document.createElement('li')
-  item.innerHTML = `${name}: ${value}`
-  return item
-}
-
-const renderList = (person) => 
-{
-  const list = document.createElement('ul')
-  Array.from(person).map((input, _i, _formElements) => {
-    if(input.value)
-    {
+  renderList(person) {
+    const list = document.createElement('ul')
+    Array.from(person).map((input, _i, _formElements) => {
+      if (input.value) {
         let value = input.value
-        if (input.type === 'color')
-        {
-            value = renderColor(value).outerHTML
+        if (input.type === 'color') {
+          value = this.renderColor(value).outerHTML
         }
-        const li = renderItem(input.name, value)
+        const li = this.renderItem(input.name, value)
         list.appendChild(li)
-  }
-  
-  })
-  return list
+      }
+    })
+    return list
+  },
+
+  handleSubmit(ev) {
+    ev.preventDefault()
+    const form = ev.target
+    const details = document.querySelector('.details')
+
+    const list = this.renderList(form.elements)
+    details.appendChild(list)
+  },
 }
 
-const handleSubmit = (ev) => {
-  ev.preventDefault()
-  const form = ev.target
-  const details = document.querySelector('.details')
-
-  /*const person = {
-    personName: form.personName.value,
-    hairColor: renderColor(form.hairColor.value).outerHTML,
-    age: form.age.value,
-    birthplace: form.birthplace.value,
-  }*/
-
-  const list = renderList(form.elements)
-  details.appendChild(list)
-}
-
-personForm.addEventListener('submit', handleSubmit)
-
-}
+App.init()
 
 
 
