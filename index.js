9d328c45799516101336f030c589478803ad30c4
Alexandra Hansma <ahansma@butler.edu>
2017-05-16 14:31:22 -0400

    Avoid hard-coding the input names in javascript

--- index.js
+++ index.js
@@ -16,9 +16,12 @@ const renderItem = (name, value) => {
 
 const renderList = (person) => {
   const list = document.createElement('ul')
-  Object.keys(person).map((key, _i, _keys) => {
-    const li = renderItem(key, person[key])
+  Array.from(person).map((input, _i, _formElements) => {
+    if(input.value)
+    {
+    const li = renderItem(input.name, input.value)
     list.appendChild(li)
+  }
   })
   return list
 }
@@ -28,14 +31,14 @@ const handleSubmit = (ev) => {
   const form = ev.target
   const details = document.querySelector('.details')
 
-  const person = {
+  /*const person = {
     personName: form.personName.value,
     hairColor: renderColor(form.hairColor.value).outerHTML,
     age: form.age.value,
     birthplace: form.birthplace.value,
-  }
+  }*/
 
-  const list = renderList(person)
+  const list = renderList(form.elements)
   details.appendChild(list)
 }
 
